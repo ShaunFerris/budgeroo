867d7e61ade01e8fddc05ad5df53a343969cc942
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-15 14:33:25 +0200

    Mockup first components and begin UI layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { FaBeer } from 'react-icons/fa';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
 const App = () => {
     return (
-        <Button variant='danger'>Test Button <FaBeer /></Button>
-    )
+        <div className='container'>
+            <h1 className='mt-3'>Budgeroo Budget Tracker</h1>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <Budget />
+                </div>
+                <div className='col-sm'>
+                    <Remaining />
+                </div>
+                <div className='col-sm'>
+                    <ExpenseTotal />
+                </div>
+            </div>
+            <h1 className='mt-3'>Expenses</h1>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ExpenseTotal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExpenseTotal = () => {
+    return (
+        <div className="alert alert-info">
+            <span>Spent so far: $800</span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
