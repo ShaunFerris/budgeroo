2805270e09e55f34e08f68ae2dc8dfaa8607c317
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-16 13:54:00 +0200

    Add context updating to AddExpenseForm, update eslint config

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Button } from "react-bootstrap";
+import { AppContext } from "../context/AppContext";
+import {v4 as uuidv4 } from "uuid";
 
 const AddExpenseForm = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInt(cost)
+        };
+
+        dispatch({
+            type: "ADD_EXPENSE",
+            payload: expense
+        });
+    };
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="row">
                 <div className="col-sm">
-                    <label for="name">Name</label>
+                    <label htmlFor="name">Name</label>
                     <input
                         className="form-control"
                         required="required"
                         type="text"
                         id="name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
                     ></input>
                 </div>
                 <div className="col-sm">
-                    <label for="cost">Cost</label>
+                    <label htmlFor="cost">Cost</label>
                     <input
                         className="form-control"
                         required="required"
                         type="text"
                         id="cost"
+                        value={cost}
+                        onChange={(event) => setCost(event.target.value)}
                     ></input>
                 </div>
                 <div className="col-sm mt-auto">
-                    <Button variant="primary" className="mt-3">Save</Button>
+                    <Button type="submit" variant="primary" className="mt-3">Save</Button>
                 </div>
             </div>
         </form>
