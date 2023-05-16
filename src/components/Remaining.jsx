3d9319e72cdf86bf46358552315b26c7d43a6e8b
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-16 14:20:02 +0200

    Add context to the Remaining component

--- src/components/Remaining.jsx
+++ src/components/Remaining.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Remaining = () => {
+    const { budget, expenses } = useContext(AppContext);
+    
+    const totalExpenses = expenses.reduce((total, expense) => {
+        return (total + expense.cost);
+    }, 0);
+
+    const alertType = totalExpenses > budget ? 
+        "alert-danger" :
+        "alert-success";
+
     return (
-        <div className="alert alert-success">
-            <span>Remaining: $200</span>
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: ${budget - totalExpenses}</span>
         </div>
     );
 };
