81dae5300d1f04c29364a3cb3055c0a62c165b0b
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-16 14:31:31 +0200

    Add context to ExpenseTotal

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext);
+    const totalSpent = expenses.reduce((total, expense) => {
+        return (total + expense.cost);
+    }, 0);
+
     return (
         <div className="alert alert-info">
-            <span>Spent so far: $800</span>
+            <span>Spent so far: ${totalSpent}</span>
         </div>
     );
 };
