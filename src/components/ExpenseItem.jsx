926ebc8a7360def3fd1f83baa58baea0ee7d9ffd
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-17 14:28:07 +0200

    Add delete expense functionality

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,6 +1,17 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 import { TiDelete } from "react-icons/ti"
 
 const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDelete = () => {
+        dispatch({
+            type: "DELETE_EXPENSE",
+            payload: props.id
+        });
+    };
+
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
             {props.name}
@@ -8,7 +19,7 @@ const ExpenseItem = (props) => {
                 <span className="mr-3 badge badge-pill bg-primary">
                     {props.cost}
                 </span>
-                <TiDelete size="1.5em"></TiDelete>
+                <TiDelete size="1.5em" onClick={handleDelete}></TiDelete>
             </div>
         </li>
     );
