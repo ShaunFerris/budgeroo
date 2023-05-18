5ac632b9be6053bef834bbbe9da59c40dc46880f
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-18 13:21:00 +0200

    Start adding budget edit functionality

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { Button } from "react-bootstrap";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
     return (
         <div className="alert alert-secondary d-flex justify-content-between p-3 align-items-center">
             <span>Budget ${budget}</span>
