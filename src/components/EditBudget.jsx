d1586eaa81d90b6038e7b8705a2a3773369d457e
Shaun Ferris <slb.ferris46@gmail.com>
2023-05-20 15:43:18 +0200

    Fix the button and input spacing on budget edit

--- src/components/EditBudget.jsx
+++ src/components/EditBudget.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 const EditBudget = (props) => {
     const [value, setValue] = useState(props.budget);
     return (
-        <>
-            <input
-                className="form-control mr-3"
-                required="required"
-                type="number"
-                id="budgetAmount"
-                value={value}
-                onChange={(event) => setValue(event.target.value)}
-            />
-            <Button
-                variant="primary"
-                onClick={() => {
-                    return props.handleSaveEdit(value)
-                }}
-            >Save</Button>
-        </>
+        <Row>
+            <Col xs={10} className="pr-3">
+                <input
+                    className="form-control mr-3"
+                    required="required"
+                    type="number"
+                    id="budgetAmount"
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
+                />
+            </Col>
+            <Col xs={2}>
+                <Button
+                    variant="primary"
+                    onClick={() => {
+                        return props.handleSaveEdit(value);
+                    }}
+                >Save</Button>
+            </Col>
+
+        </Row>
     );
 };
 
